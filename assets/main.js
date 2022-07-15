f6b6f58499cfb5a2dd00e397fe59d1238a0e039c
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2022-07-14 19:59:11 -0500

    V1.7

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,12 @@
-
+const nav = document.querySelector('.nav-menu');
 
 document.addEventListener('click', function(e) {
     if (e.target.matches('.fa-bars')) {
-        alert('hoa')
+
+        if (nav.style.display === 'flex') {
+                nav.style.display = 'none';
+        } else {
+                nav.style.display = 'flex';
+        }
     }
 });
